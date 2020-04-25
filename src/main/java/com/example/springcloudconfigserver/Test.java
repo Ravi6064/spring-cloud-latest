02aa766c3376f76a267cac43b37a855811ddd69b
Ravi <Kiran>
2020-04-25 14:46:33 +0530

    Changed log msg

--- src/main/java/com/example/springcloudconfigserver/Test.java
+++ src/main/java/com/example/springcloudconfigserver/Test.java
@@ -3,6 +3,6 @@ package com.example.springcloudconfigserver;
 public class Test {
 
 	public void test() {
-		System.out.println(" Test method added in branch");
+		System.out.println(" Master TEst method added in branch");
 	}
 }
